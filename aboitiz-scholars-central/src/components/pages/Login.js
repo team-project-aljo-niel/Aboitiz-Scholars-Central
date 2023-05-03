d20667af94d2db0b0b24cc8f4154f5718bc756e4
Niel <dmendegorin@gmail.com>
2023-05-03 20:57:54 +0800

    test: dummy login

--- aboitiz-scholars-central/src/components/pages/Login.js
+++ aboitiz-scholars-central/src/components/pages/Login.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div><button><Link to="/ASC/dashboard">Login</Link></button></div>
   )
 }
 
