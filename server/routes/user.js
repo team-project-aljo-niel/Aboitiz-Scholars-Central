28b95c5f494a1b309a14b7b6c892fea0f90c5f5d
AljoGabinete <114290303+AljoGabinete@users.noreply.github.com>
2023-05-08 21:50:12 +0800

    Edited scholar details

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 const authChecker = require('../auth/authChecker');
-const cookieParser = require('cookie-parser');
 
-router.use(cookieParser());
 router.get('/', authChecker, userController.getAllUsers);
 router.get('/details', authChecker, userController.getCurrentUser);
 router.put('/details', authChecker, userController.changeUserDetails);
